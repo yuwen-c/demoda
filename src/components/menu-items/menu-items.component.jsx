8ee1b76bbfdb3bfa6f803e435134bdb350ac49ee
yuwen-c <yuwen.azulejos@gmail.com>
2022-04-24 15:16:06 +0800

    feat: add bg-img for menu-item

--- src/components/menu-items/menu-items.component.jsx
+++ src/components/menu-items/menu-items.component.jsx
@@ -3,14 +3,13 @@ import "./menu-items.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-      }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
