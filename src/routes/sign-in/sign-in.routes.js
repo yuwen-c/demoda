f4f330dca8eb09fe1440c93dd6f75d3e3f887d16
yuwen <yuwen.azulejos@gmail.com>
2022-07-12 15:21:31 +0800

    feat: use firebase services in authentication and store user data

--- src/routes/sign-in/sign-in.routes.js
+++ src/routes/sign-in/sign-in.routes.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 // 在這邊做一個登入按鈕，可彈出三方登入的視窗
 const SignIn = () => {
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
-    console.log(response);
+    const { user } = response;
+    createUserDocumentFromAuth(user);
   };
   return (
     <div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {
   getAuth,
@@ -8,8 +6,8 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBoroqo2BRmbixmkj2JeuefMmHvAKgGpcU",
   authDomain: "demoda-db.firebaseapp.com",
@@ -22,12 +20,43 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// authentication service
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: "select account",
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => {
   return signInWithPopup(auth, provider);
+};
+
+// firestore service
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  // here userAuth means an user that pass the authentication
+  // check if the db ref exists: "user" collection, and the unique key is uid
+  const usersDocRef = doc(db, "users", userAuth.uid);
+
+  // snapShot: get data from pointing to the ref
+  const userSnapShot = await getDoc(usersDocRef);
+
+  // save user's data if it doesn't exist
+  if (!userSnapShot.exists()) {
+    /**
+     * 這幾個user資料只有這個block要用到，寫在if裡面就好，不要寫在外面。
+     */
+    const { displayName, email } = userAuth;
+    const createDate = new Date();
+    try {
+      await setDoc(usersDocRef, {
+        displayName,
+        email,
+        createDate,
+      });
+    } catch (error) {
+      console.log("error in creating new user", error);
+    }
+  }
+  return usersDocRef;
 };
