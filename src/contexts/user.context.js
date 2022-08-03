c739ec05fd4a0d8fbda2575ec7a2d1de82f20f9b
yuwen <yuwen.azulejos@gmail.com>
2022-08-03 11:35:03 +0800

    refac: apply useReducer in uerContext to replace useState

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,21 +1,45 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
-// actual value you can access
+/** a useReducer version */
+
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => {},
 });
 
-// a provider function that returns a context.Provider, which will wrap App
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (initState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...initState,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`unhandled type ${type} in userReducer`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState();
+  const [updatedState, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const setCurrentUser = (user) => {
+    const action = {
+      type: USER_ACTION_TYPES.SET_CURRENT_USER,
+      payload: user,
+    };
+    dispatch(action);
+  };
 
   // subscribe the auth state listener
-  /**
-   * useEffect callback function only executes once after mounting (since [])
-   * then the listener keeps working
-   */
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       setCurrentUser(user);
@@ -24,6 +48,7 @@ export const UserProvider = ({ children }) => {
     return unsubscribe;
   }, []);
 
+  const { currentUser } = updatedState;
   const value = { currentUser, setCurrentUser };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
---
+++ src/contexts/user.context_useState.js
@@ -0,0 +1,30 @@
+import { createContext, useState, useEffect } from "react";
+import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
+
+// actual value you can access
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => {},
+});
+
+// a provider function that returns a Context.Provider, which will wrap App
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
+
+  // subscribe the auth state listener
+  /**
+   * useEffect callback function only executes once after mounting (since [])
+   * then the listener keeps working
+   */
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      setCurrentUser(user);
+    });
+    // clean up function, will be triggered when un-mount
+    return unsubscribe;
+  }, []);
+
+  const value = { currentUser, setCurrentUser };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
